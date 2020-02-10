8613a849598c9fbabf794a3672cea54d6e7a3485
kosadchij <kosadchij@inbox.ru>
2020-02-11 01:18:32 +0300

    menu items seeder edited

--- database/seeds/MenuItemsTableSeeder.php
+++ database/seeds/MenuItemsTableSeeder.php
@@ -9,23 +9,66 @@ class MenuItemsTableSeeder extends Seeder
         [
             'name' => 'Pepperoni',
             'description' => 'Perfect combination: tender chicken fillet with blue cheese crumbles, Parmeggiano sauce, mix of Italian cheeses and Mozzarella cheese',
-            'image' => ''
+            'image' => '/images/pepperoni.jpg'
         ],
         [
             'name' => 'Margherita',
             'description' => 'Perfect combination: tender chicken fillet with blue cheese crumbles, Parmeggiano sauce, mix of Italian cheeses and Mozzarella cheese',
-            'image' => ''
+            'image' => '/images/margherita.jpeg'
         ],
         [
             'name' => 'Carbonara',
             'description' => 'Perfect combination: tender chicken fillet with blue cheese crumbles, Parmeggiano sauce, mix of Italian cheeses and Mozzarella cheese',
-            'image' => ''
+            'image' => '/images/carbonara.jpg'
         ],
         [
             'name' => 'Napoletana',
             'description' => 'Perfect combination: tender chicken fillet with blue cheese crumbles, Parmeggiano sauce, mix of Italian cheeses and Mozzarella cheese',
-            'image' => ''
+            'image' => '/images/napoletana.jpg'
         ],
+        [
+            'name' => 'Chicken BBQ',
+            'description' => 'Perfect combination: tender chicken fillet with blue cheese crumbles, Parmeggiano sauce, mix of Italian cheeses and Mozzarella cheese',
+            'image' => '/images/chicken_bbq.jpg'
+        ],
+        [
+            'name' => 'Ham and mushrooms',
+            'description' => 'Perfect combination: tender chicken fillet with blue cheese crumbles, Parmeggiano sauce, mix of Italian cheeses and Mozzarella cheese',
+            'image' => '/images/ham_and_mushrooms.jpg'
+        ],
+        [
+            'name' => 'Chicken ranch',
+            'description' => 'Perfect combination: tender chicken fillet with blue cheese crumbles, Parmeggiano sauce, mix of Italian cheeses and Mozzarella cheese',
+            'image' => '/images/chicken_ranch.jpg'
+        ],
+        [
+            'name' => 'Meat',
+            'description' => 'Perfect combination: tender chicken fillet with blue cheese crumbles, Parmeggiano sauce, mix of Italian cheeses and Mozzarella cheese',
+            'image' => '/images/meat.jpg'
+        ],
+        [
+            'name' => 'Alfredo',
+            'description' => 'Perfect combination: tender chicken fillet with blue cheese crumbles, Parmeggiano sauce, mix of Italian cheeses and Mozzarella cheese',
+            'image' => '/images/Alfredo.jpg'
+        ]
+    ];
+
+    const DRINKS_ITEMS = [
+        [
+            'name' => 'Coca-cola',
+            'description' => 'Soft drink',
+            'image' => '/images/coke.jpg'
+        ],
+        [
+            'name' => 'Fanta',
+            'description' => 'Soft drink',
+            'image' => '/images/fanta.jpeg'
+        ],
+        [
+            'name' => 'Sprite',
+            'description' => 'Soft drink',
+            'image' => '/images/sprite.jpg'
+        ]
     ];
 
     const OPTIONS = [
@@ -43,6 +86,21 @@ class MenuItemsTableSeeder extends Seeder
         ],
     ];
 
+    const DRINKS_OPTIONS = [
+        [
+            'name' => '0.5 L',
+            'price' => '1'
+        ],
+        [
+            'name' => '1 L',
+            'price' => '1.2'
+        ],
+        [
+            'name' => '1.5 L',
+            'price' => '1.5'
+        ],
+    ];
+
     /**
      * Run the database seeds.
      *
@@ -52,6 +110,9 @@ class MenuItemsTableSeeder extends Seeder
     {
         foreach (self::ITEMS as $item) {
             MenuItem::create($item)->options()->createMany(self::OPTIONS);
+        }
+        foreach (self::DRINKS_ITEMS as $item) {
+            MenuItem::create($item)->options()->createMany(self::DRINKS_OPTIONS);
         }
     }
 }
